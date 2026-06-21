2762ec4cf79a6b6bf2627457295b84df5ee05e4a
KirtKurt <kurt.silvers@gmail.com>
2026-06-21 10:41:05 -0400

    Add member card page

---
+++ frontend/app/community/inqsi-member/page.tsx
@@ -0,0 +1,9 @@
+import { AppHeader } from '@/components/AppHeader';
+
+export default function Page() {
+  return (
+    <main className="shell">
+      <AppHeader title="Member Card" />
+    </main>
+  );
+}
