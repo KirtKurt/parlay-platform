a25cf3e527ed9e8f03cb5d8bed03bd206273e8c1
KirtKurt <kurt.silvers@gmail.com>
2026-08-13 04:53:55 -0400

    Add inning market classification regression coverage

--- mlb_auto/tests/test_runtime_autonomy.py
+++ mlb_auto/tests/test_runtime_autonomy.py
@@ -1,3 +1,4 @@
+from mlb_auto.autonomous_handler import _market_categories
 from mlb_auto.ml import Model
 from mlb_auto.provider_open import OpenEndedOddsApiClient
 from mlb_auto.schedule_controller import desired_interval_minutes
@@ -25,3 +26,34 @@ def test_market_change_can_force_five_minute_cadence_far_from_first_pitch():
     assert desired_interval_minutes(30, missing_market_fraction=.40) == 5
     assert desired_interval_minutes(30, new_event_fraction=.40) == 5
     assert desired_interval_minutes(30, recent_signal_change=.03) == 5
+
+
+def test_inning_markets_are_classified_as_period_without_affecting_other_families():
+    keys = [
+        'h2h',
+        'h2h_1st_inning',
+        'totals_1st_3_innings',
+        'spreads_1st_5_innings',
+        'h2h_1st_7_innings',
+        'alternate_spreads_1st_5_innings',
+        'pitcher_strikeouts',
+        'batter_hits',
+        'team_totals',
+        'alternate_totals',
+        'totally_new_mlb_market_2026',
+    ]
+    categories = _market_categories(keys)
+
+    assert categories['featured'] == ['h2h']
+    assert categories['period'] == [
+        'alternate_spreads_1st_5_innings',
+        'h2h_1st_7_innings',
+        'h2h_1st_inning',
+        'spreads_1st_5_innings',
+        'totals_1st_3_innings',
+    ]
+    assert categories['pitcher'] == ['pitcher_strikeouts']
+    assert categories['batter'] == ['batter_hits']
+    assert categories['team_total'] == ['team_totals']
+    assert categories['alternate'] == ['alternate_totals']
+    assert categories['other'] == ['totally_new_mlb_market_2026']
