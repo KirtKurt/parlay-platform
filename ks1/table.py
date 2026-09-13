1321345320d6a14000289f01090c840bc8e86580
KirtKurt <kurt.silvers@gmail.com>
2026-09-12 20:58:12 -0400

    Restore main KS1 table.py (unbreak branch)

--- ks1/table.py
+++ ks1/table.py
@@ -11,7 +11,5 @@ import pyarrow as pa
 
 from ks1.features import Features, day, number, utc
 from ks1.inventory import encode
-from ks1.refresh import pregame_status
-from ks1.starter_identity import published_starter_index
 
 VERSION = "KS1-game-table-v1"
